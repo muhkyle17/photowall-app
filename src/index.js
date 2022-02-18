db57c2c5b57ff1bdb2d1a27ed78269a6c23369e5
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-02-19 00:23:12 +0800

    Refactor code to make it more organized and clean

--- src/index.js
+++ src/index.js
@@ -1,36 +1,5 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-
-class List extends Component {
-  render() {
-    return (
-      <div>
-        <ol>
-          {this.props.tasks.map((task, index) => (
-            <li key={index}>{task}</li>
-          ))}
-        </ol>
-      </div>
-    )
-  }
-}
-
-class Title extends Component {
-  render() {
-    return <h1>{this.props.title}</h1>
-  }
-}
-
-class Main extends Component {
-  render() {
-    return (
-      <div>
-        <Title title={['To-dos']} />
-        <List tasks={['Mow the lawn', 'walk the dog']} />
-        <List tasks={['hose the driveway', 'finish the laundry']} />
-      </div>
-    )
-  }
-}
+import Main from './components/Main'
 
 ReactDOM.render(<Main />, document.getElementById('root'))
